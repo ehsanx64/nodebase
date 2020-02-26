93650e66d2bb43190fb33a22973c07c2b4085daa
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-02-26 23:07:57 +0330

    Added Module1

---
+++ src/Module1/index.js
@@ -0,0 +1,45 @@
+var Module1 = function () {
+    this.name = "Module1";
+
+    this.load = function (express) {
+        var that = this;
+        this.app = express;
+        this.setRoutes();
+    };
+
+    this.setRoutes = function () {
+        var that = this;
+
+        this.app.get('/mod1', function (req, res) {
+            res.end(that.loadMessage())
+        })
+
+        this.app.get('/mod1/hello', function (req, res) {
+            res.send(that.hello())
+        })
+    }
+
+    this.hello = function () {
+        return 'Hello World from ' + this.getName()
+    }
+    
+    this.getName = function () {
+        return this.name
+    };
+    
+    this.getInfo = function () {
+        return {
+            name: getName()
+        }
+    };
+
+    this.loadMessage = function () {
+        return `${this.getName()} loaded!`
+    };
+}
+
+module.exports = function (i) {
+    var obj = new Module1();
+    obj.load(i);
+    return obj;
+}
--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mod1 = require('./Module1')
+const mods = [
+    mod1
+]
+
+for (var i in mods) {
+    mods[i](app)
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
