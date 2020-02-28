5918a7fd65e1c144b38d8cb5f08f2d7501942b9b
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-02-29 01:26:53 +0330

    Added a data collection

---
+++ src/data.js
@@ -0,0 +1,14 @@
+module.exports.data = {[
+    {
+        name: "Adam Smith",
+        age: 30
+    },
+    {
+        name: "George Hart",
+        age: 60
+    },
+    {
+        name: "Eli Jackson",
+        age: 20
+    },
+]};
